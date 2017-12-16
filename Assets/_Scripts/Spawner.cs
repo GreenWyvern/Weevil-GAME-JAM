370a5870ea6765291f873b3e8cd2c5ce5921711d
GreenWyvern <gamevingw@gmail.com>
2017-12-16 10:39:30 -0500

    Added Ship Spawning

---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnOne());
+        StartCoroutine(SpawnTwo());
+        StartCoroutine(SpawnThree());
+        StartCoroutine(SpawnFour());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnOne()
+    {
+        
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            Vector2 position = new Vector2(Random.Range(-13, 13), Random.Range(15, 15));
+
+            Instantiate(enemy, position, this.transform.rotation);
+        }
+    }
+
+    IEnumerator SpawnTwo()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            Vector2 position = new Vector2(Random.Range(-13, 13), Random.Range(-15, -15));
+
+            Instantiate(enemy, position, this.transform.rotation);
+        }
+    }
+
+    IEnumerator SpawnThree()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            Vector2 position = new Vector2(Random.Range(15, 15), Random.Range(-15, 15));
+
+            Instantiate(enemy, position, this.transform.rotation);
+        }
+    }
+
+    IEnumerator SpawnFour()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            Vector2 position = new Vector2(Random.Range(-15, -15), Random.Range(-15, 15));
+
+            Instantiate(enemy, position, this.transform.rotation);
+        }
+    }
+
+}
